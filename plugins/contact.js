537de34aab8c126348b8533eae670ceafac1d26d
Aj-fx <94107120+Aj-fx@users.noreply.github.com>
2021-11-19 13:12:46 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -11,7 +11,7 @@ const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Ajfx [OWNER]\n' // full name
             + 'ORG:Ajfx;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=918281440157:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid=918281440156:+91 8289968468\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "Ajfx [OWNER]", vcard: vcard}, MessageType.contact)
 }))
