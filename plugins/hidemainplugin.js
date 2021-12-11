9bbb54ad725d2fd99c233c98ed9387307447f3ed
Aj-fx <94107120+Aj-fx@users.noreply.github.com>
2021-12-11 20:58:04 +0530

    Update hidemainplugin.js

--- plugins/hidemainplugin.js
+++ plugins/hidemainplugin.js
@@ -26,7 +26,7 @@ Amalser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (a
       
       const buttonMessage = {
           contentText: '```'+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n',
-          footerText: 'ᴀᴊ ғx💕 ✝︎',
+          footerText: 'ᴀᴊ ғx💕 ✮',
           buttons: buttons,
           headerType: 1
       }
